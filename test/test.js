6085b12ff39758d8c41e30e8e8c617d54519fa36
Daniel Johansson <daniel.johansson@ymail.com>
2012-11-24 11:23:11 -0500

    test

---
+++ test/test.js
@@ -0,0 +1,37 @@
+var approx = function (a, b, tol) {
+    tol || (tol = 1e-2);
+    return Math.abs(a - b) < tol;
+};
+
+var tests = [{
+        name: 'nderiv (undefined order) exp',
+        test: approx.bind(this, nderiv(Math.exp, 3, 1e-2), Math.exp(3))
+    }, {
+        name: 'nderiv (order 4 == undef) sin',
+        test: function () { return nderiv(Math.sin, 3, 1e-4, 4) === nderiv(Math.sin, 3, 1e-4)}
+    }, {
+        name: 'nderiv (order 1) exp',
+        test: approx.bind(this, nderiv(Math.exp, 3, 1e-4, 1), Math.exp(3))
+    }, {
+        name: 'nderiv (order 2) exp',
+        test: approx.bind(this, nderiv(Math.exp, 3, 1e-3, 2), Math.exp(3))
+    }, {
+        name: 'nderiv (order 6) exp',
+        test: approx.bind(this, nderiv(Math.exp, 3, 1e-1, 6), Math.exp(3))
+    }
+];
+
+var testList = document.querySelector('#results');
+
+tests.forEach(function (test) {
+    var result = document.createElement('li');
+    if (test.test()) {
+        result.textContent = 'Success - ' + test.name;
+        result.classList.add('success');
+    }
+    else {
+        result.textContent = 'Faliure - ' + test.name;
+        result.classList.add('faliure');
+    }
+    testList.appendChild(result);
+});
