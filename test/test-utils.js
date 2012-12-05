94d749bc2e4c9f94bda40f36d69a06daec464e87
Daniel Johansson <daniel.johansson@ymail.com>
2012-12-05 18:55:20 -0500

    added some tests

--- test/test-utils.js
+++ test/test-utils.js
@@ -1,25 +1,78 @@
-var testList = document.querySelector('#results');
-
-var runTests = function (name, tests) {
+var Test = (function () {
     
-    var li = document.createElement('li');
-    var ul = document.createElement('ul');
+    // Approximately equal to (with configurable tolerance)
+    var approx = function (a, b, tol) {
+        tol || (tol = 1e-2);
+        return Math.abs(a - b) < tol;
+    };
     
-    li.textContent = name;
-    
-    tests.forEach(function (test) {
-        var result = document.createElement('li');
-        if (test.test()) {
-            result.textContent = 'Success - ' + test.name;
-            result.classList.add('success');
+    // Compare matrices
+    var matrixApprox = function (A, B, tol) {
+        var i, j;
+        
+        if (A.length !== B.length || A[0].length !== B[0].length) {
+            return false;
         }
-        else {
-            result.textContent = 'Faliure - ' + test.name;
-            result.classList.add('faliure');
+        
+        for (i = 0; i < A.length; i++) {
+            for (j = 0; j < A[0].length; j++) {
+                if (!approx(A[i][j], B[i][j], tol)) {
+                    return false;
+                }
+            }
         }
-        ul.appendChild(result);
-    });
+        return true;
+    };
     
-    li.appendChild(ul);
-    testList.appendChild(li);
-};
+    // Compare arrays
+    var arrayApprox = function (a, b, tol) {
+        var i;
+        
+        if (a.length !== b.length) {
+            return false;
+        }
+        
+        for (i = 0; i < a.length; i++) {
+            if (!approx(a[i], b[i], tol)) {
+                return false;
+            }
+        }
+        return true;
+    };
+    
+    
+    
+    var testList = document.querySelector('#results');
+    
+    var runTests = function (name, tests) {
+        
+        var li = document.createElement('li');
+        var ul = document.createElement('ul');
+        
+        li.textContent = name;
+        
+        tests.forEach(function (test) {
+            var result = document.createElement('li');
+            if (test.test()) {
+                result.textContent = 'Success - ' + test.name;
+                result.classList.add('success');
+            }
+            else {
+                result.textContent = 'Faliure - ' + test.name;
+                result.classList.add('faliure');
+            }
+            ul.appendChild(result);
+        });
+        
+        li.appendChild(ul);
+        testList.appendChild(li);
+    };
+    
+    return {
+        run: runTests,
+        approx: approx,
+        matrixApprox: matrixApprox,
+        arrayApprox: arrayApprox
+    };
+    
+})();
--- test/tests.js
+++ test/tests.js
@@ -1,27 +1,61 @@
 var nderivTests = [{
-        name: 'nderiv (undefined order ==> 4) sin',
-        test: function () { return nderiv(Math.sin, 3, 1e-4) === nderiv(Math.sin, 3, 1e-4)}
-    }, {
         name: 'nderiv (order 1) exp',
-        test: approx.bind(this, nderiv(Math.exp, 3, 1e-4, 1), Math.exp(3))
+        test: Test.approx.bind(this, nderiv(Math.exp, 3, 1e-4, 1), Math.exp(3))
     }, {
         name: 'nderiv (order 2) exp',
-        test: approx.bind(this, nderiv(Math.exp, 3, 1e-3, 2), Math.exp(3))
+        test: Test.approx.bind(this, nderiv(Math.exp, 3, 1e-3, 2), Math.exp(3))
     }, {
         name: 'nderiv (order 4) exp',
-        test: approx.bind(this, nderiv(Math.exp, 3, 1e-2, 4), Math.exp(3))
+        test: Test.approx.bind(this, nderiv(Math.exp, 3, 1e-2, 4), Math.exp(3))
     }, {
         name: 'nderiv (order 6) exp',
-        test: approx.bind(this, nderiv(Math.exp, 3, 1e-1, 6), Math.exp(3))
+        test: Test.approx.bind(this, nderiv(Math.exp, 3, 1e-1, 6), Math.exp(3))
+    }, {
+        name: 'nderiv (undefined order == 4) sin',
+        test: function () { return nderiv(Math.sin, 3, 1e-4) === nderiv(Math.sin, 3, 1e-4)}
     }
 ];
-runTests('nderiv', nderivTests);
+Test.run('nderiv', nderivTests);
 
 var linalgTests = [{
+        name: 'vector norm',
+        test: Test.approx.bind(this, linalg.norm([1,2,-3, 1, -1]), 4)
+    }, {
         name: 'transpose',
         test: function () {
-            return false;
+            var A = [[1,2,3],[4,5,6],[7,8,9]];
+            var T = [[1,4,7],[2,5,8],[3,6,9]];
+            return Test.matrixApprox(linalg.transpose(A), T);
+        }
+    }, {
+        name: 'matrix vector multiplication',
+        test: function () {
+            var A = [[1,2],[3,4]];
+            var b = [5,6];
+            var c = [17,39];
+            return Test.arrayApprox(linalg.mvMult(A, b), c);
+        }
+    }, {
+        name: 'matrix matrix multiplication',
+        test: function () {
+            var A = [[1,-2,3],[-4,5,-6]];
+            var B = [[-1,2],[-3,4],[-5,6]];
+            var C = [[-10,12],[19,-24]];
+            return Test.matrixApprox(linalg.mmMult(A, B), C);
         }
     }
 ];
-runTests('linalg', linalgTests);
+Test.run('linalg', linalgTests);
+
+var linsolveTests = [{
+        name: 'linsolve',
+        test: function () {
+            var A = [[1,2],[3,4]];
+            var b = [17,39];
+            // Ax = B ==> x = [5, 6]
+            x = linsolve(A, b);
+            return Test.approx(x[0], 5) && Test.approx(x[1], 6);
+        }
+    }
+];
+Test.run('linsolve', linsolveTests);
