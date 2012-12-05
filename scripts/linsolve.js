7d9d54f91651c3126ea56f0aeaf67f74b3405fbb
Daniel Johansson <daniel.johansson@ymail.com>
2012-12-05 15:32:11 -0500

    moved test to tests

--- scripts/linsolve.js
+++ scripts/linsolve.js
@@ -1,10 +1,4 @@
 
-var A = [[1,2],[3,4]];
-var b = [17,39];
-// Ax = B ==> x = [5, 6]
-x = linsolve(A, b);
-console.log(approx(x[0], 5) && approx(x[1], 6));
-
 // singular
 //x = linsolve([[1,2,3,4,5],[6,7,8,9,0],[3,4,5,6,7],[2,4,5,6,7],[5,6,7,8,9]], [1,3,5,7,8]); 
 //console.log(x)
